6e108312bf497f1da97d512383b6fabf08e5c606
Caroline <caroline.callens90@gmail.com>
2018-10-25 08:17:45 +0200

    Created User Class

---
+++ Digibooky/Digibooky.Domain/Users/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digibooky.Domain.Users
+{
+    public class User
+    {
+        public enum Roles { member }
+
+        public Guid ID { get; }
+        public int INSS { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public Roles UserRole { get; private set; }
+        public string Street { get; private set; }
+        public string StreetNumber { get; private set; }
+        public int PostalCode { get; private set; }
+        public string City { get; private set; }
+    }
+}
