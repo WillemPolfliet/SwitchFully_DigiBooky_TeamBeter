ea79715213aee3b9e6b362baf29832cc325fc992
Gwen Jamroziak <gwen.jamroziak@gmail.com>
2018-10-29 08:51:18 +0100

    moved class out of interface folder

--- Digibooky/Digibooky.API/Controllers/Lendings/LendingDTOLendingPost.cs
+++ Digibooky/Digibooky.API/Controllers/Lendings/LendingDTOLendingPost.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Digibooky.API.Controllers.Lendings.Interfaces
+namespace Digibooky.API.Controllers.Lendings
 {
 	public class LendingDTOLendingPost
 	{
